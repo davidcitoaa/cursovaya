37873d0e307d4cc08fdcd53b1f815fcf4f0a8d56
David <90219589+davidcitoaa@users.noreply.github.com>
2024-01-10 15:31:09 +0300

    Добавил отображение вкладов. Исправил неточности.

--- app.py
+++ app.py
@@ -1,23 +1,6 @@
-# для того, чтобы код работал, нужно, чтобы была такая таблица:
-# CREATE TABLE IF NOT EXISTS public.deposit
-# (
-#     deposit_id integer NOT NULL DEFAULT nextval('deposit_deposit_id_seq'::regclass),
-#     client_id integer NOT NULL,
-#     storage_period date NOT NULL,
-#     loan_id integer,
-#     interest_capitalization_frequency character varying(255) COLLATE pg_catalog."default" NOT NULL,
-#     CONSTRAINT deposit_pkey PRIMARY KEY (deposit_id),
-#     CONSTRAINT deposit_client_id_fkey FOREIGN KEY (client_id)
-#         REFERENCES public.client (client_id) MATCH SIMPLE
-#         ON UPDATE NO ACTION
-#         ON DELETE NO ACTION,
-#     CONSTRAINT deposit_loan_id_fkey FOREIGN KEY (loan_id)
-#         REFERENCES public.loan (loan_id) MATCH SIMPLE
-#         ON UPDATE NO ACTION
-#         ON DELETE NO ACTION
-# )
-
 import hashlib
+from datetime import datetime, timedelta
+
 from flask import Flask, render_template, redirect, url_for
 from peewee import *
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -118,6 +101,39 @@ class RegisterForm(FlaskForm):
 
 
 class DepositForm(FlaskForm):
+    duration = SelectField(
+        'Срок вклада',
+        choices=[
+            (3, '3 месяца'),
+            (4, '4 месяца'),
+            (5, '5 месяцев'),
+            (6, '6 месяцев'),
+            (7, '7 месяцев'),
+            (8, '8 месяцев'),
+            (9, '9 месяцев'),
+            (10, '10 месяцев'),
+            (11, '11 месяцев'),
+            (12, '12 месяцев'),
+            (13, '13 месяцев'),
+            (14, '14 месяцев'),
+            (15, '15 месяцев'),
+            (16, '16 месяцев'),
+            (17, '17 месяцев'),
+            (18, '18 месяцев'),
+            (19, '19 месяцев'),
+            (20, '20 месяцев'),
+            (21, '21 месяцев'),
+            (22, '22 месяцев'),
+            (23, '23 месяцев'),
+            (24, '24 месяца'),
+        ],
+        validators=[DataRequired()]
+    )
+    amount = DecimalFieldHTML5('Amount', validators=[DataRequired()])
+    submit = SubmitField('Создать')
+
+
+class DepositForm1(FlaskForm):
     storage_period = DateField(validators=[DataRequired()])
     interest_capitalization_frequency = SelectField(
         'Период капитализации процентов',
@@ -147,6 +163,8 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    for error in form.errors:
+        print(error)
 
     if form.validate_on_submit():
         user = Client.get_or_none(Client.email == form.username.data)
@@ -181,8 +199,8 @@ def register():
 
 # Определение формы для пополнения баланса
 class AddFundsForm(FlaskForm):
-    amount = DecimalFieldHTML5('Amount', validators=[DataRequired()])
-    submit = SubmitField('Add Funds')
+    amount = DecimalFieldHTML5('Введите сумму', validators=[DataRequired()])
+    submit = SubmitField('Пополнить')
 
 
 # Маршрут для пополнения баланса
@@ -213,9 +231,22 @@ def add_funds():
             user_balance.save()
 
         return redirect(url_for('dashboard'))
+    else:
+        for error in form.errors:
+            print(error)
+    user_balance = getClientBalance()
 
-    return render_template('add_funds.html', form=form)
+    return render_template('add_funds.html', form=form, current_balance=user_balance)
 
+
+def getClientBalance():
+    client_balance = ClientBalance.get_or_none(client_id=current_user.client_id)
+    if client_balance is None:
+        client_balance = ClientBalance.create(client_id=current_user, balance=0.0)
+        client_balance.save()
+
+    balance_value = client_balance.balance
+    return balance_value
 
 @app.route('/dashboard')
 @login_required
@@ -228,6 +259,10 @@ def dashboard():
         client_balance.save()
 
     balance_value = client_balance.balance
+
+    # Получаем информацию о депозитах пользователя
+    user_deposits = Deposit.select().join(Loan).where(Deposit.client_id == current_user)
+    print(user_deposits)
 
     # Получаем логин клиента
     name = user.full_name
@@ -247,7 +282,8 @@ def dashboard():
                            name=name,
                            date_of_birth=date_of_birth,
                            email=email,
-                           phone_number=phone)
+                           phone_number=phone,
+                           deposits=user_deposits)
 
 
 @app.route('/logout', methods=['POST'])
@@ -257,44 +293,57 @@ def logout():
     return redirect(url_for('index'))
 
 
-
-@app.route('/create_deposit', methods=['POST'])
+@app.route('/create_deposit', methods=['GET', 'POST'])
 @login_required
 def create_deposit():
     form = DepositForm()
-
+    user_balance = getClientBalance()
     if form.validate_on_submit():
         amount = float(form.amount.data)
-        interest_rate = float(form.interest_rate.data)
-        loan = Loan.create(
-            amount=amount,  # Получаем значение поля формы
-            interest_rate=interest_rate
-        )
-        loan_id = loan
-        print(loan_id)
 
-        client_id = current_user
-        storage_period = form.storage_period.data
-        interest_capitalization_frequency = form.interest_capitalization_frequency.data
+        # Проверка, что сумма депозита меньше баланса пользователя
+        if user_balance < amount:
+            return render_template('create_deposit.html', form=form, error="Недостаточно средств на балансе", current_balance=user_balance)
 
-        print(client_id, storage_period, interest_capitalization_frequency)
+        # Вычитаем сумму депозита из баланса пользователя
+        user_balance = current_user.balance.get()
+
+        # Получаем значение баланса из объекта
+        current_balance = user_balance.balance  # Доступ к реальному значению баланса
+
+        # Обновляем баланс пользователя
+        updated_balance = current_balance - amount
+        user_balance.balance = updated_balance
+        user_balance.save()
+
+        duration = int(form.duration.data)
+        # Определение процентной ставки в зависимости от срока
+        if duration <= 12:
+            interest_rate = 10.0
+        else:
+            interest_rate = 15.0
+
+        # Определение даты окончания депозита
+        end_date = datetime.now() + timedelta(days=duration * 30)
+
+        # Создание займа
+        loan = Loan.create(amount=amount, interest_rate=interest_rate)
+
+        # Создание депозита
         deposit = Deposit.create(
-            client_id=client_id,
-            storage_period=storage_period,
-            interest_capitalization_frequency=interest_capitalization_frequency,
-            loan_id=loan_id,
+            client_id=current_user,
+            storage_period=end_date,
+            interest_capitalization_frequency='1 месяц',
+            loan_id=loan,
         )
         deposit.save()
 
         return redirect(url_for('dashboard'))
     else:
-        # Данные некорректны, выводим ошибки
         for error in form.errors:
             print(error)
 
-        return render_template('create_deposit.html', form=form)
-
-    # return render_template('create_deposit.html', form=form)
+        return render_template('create_deposit.html', form=form, current_balance=user_balance)
 
 
 if __name__ == '__main__':
